a221fa3daa6c3f70731e14af4a3bfbcaaff54dbf
Cédric <cestrebe@ulb.ac.be>
2013-12-19 19:03:40 +0100

    [src] Fixed Ball

--- code/Ball.h
+++ code/Ball.h
@@ -1,22 +1,16 @@
 #ifndef Ball_h
 #define Ball_h
 
-#include "Match.h"
-
-class Match;
-
 class Ball {
 
- public:
-	virtual Ball()
-	virtual ~Ball()
-	virtual bool operator=(const Ball &ball)
-    virtual void move();
-    virtual int getSpeed();
-    virtual void setSpeed();
+	public:
+		virtual Ball();
+		virtual ~Ball();
+		virtual bool operator=(const Ball &ball)
+		virtual void move();
+		virtual int getSpeed();
 
- private:
-    int speed;
-    Match *myMatch;
+	private:
+		int speed;
 };
 #endif // Ball_h

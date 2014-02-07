1c3983a55fd56131d23286e76867bc2242b8ed8c
Bruno Rocha Pereira <brochape@ulb.ac.be>
2014-02-07 18:17:12 +0100

    [code] Added the cpp for Match

--- src/Match.h
+++ src/Match.h
@@ -1,30 +1,30 @@
 #ifndef MATCH_H
 #define MATCH_H
 
-#include "Team.h"
+#include "Club.h"
 #include "Field.h"
 #include "GoldenSnitch.h"
 #include "Quaffle.h"
 #include "Budger.h"
 #include "Case.h"
 
-static const width = 1;
-static const length = 1;
+enum { WIDTH = 1, LENGHT = 1};
+
 class Match{
 
     public:
-        Match(Club & host, Club & guest);
+        Match(Club& host, Club& guest);
         ~Match();
         int* getScore();
         int addPoint(bool guestTeam ,int delta = 1);
 
     private:
-        int score[2];
-        Club *club[2];
-        Case grid[width][length];
-        GoldenSnitch goldenSnitch;
-        Quaffle quaffle;
-        Budger budgers[2];
+        int score_[2];
+        Club *clubs_[2];
+        Case grid_[WIDTH][LENGHT];
+        GoldenSnitch goldenSnitch_;
+        Quaffle quaffle_;
+        Budger budgers_[2];
 };
 
 #endif // MATCH_H

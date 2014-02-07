8967db9abffdbe59c5d9bb75ebbf4a42c2780232
Bruno Rocha Pereira <brochape@ulb.ac.be>
2014-02-07 17:26:23 +0100

    [code] added cpp for Installation and Manager

---
+++ src/Manager.cpp
@@ -0,0 +1,32 @@
+#include "Manager.h"
+using namespace std;
+
+Manager::Manager(string name, string userName, string password, Club club = Club()): name_(name), userName_(userName), club_(club){
+	//TODO : Hash du password
+}
+
+Manager::~Manager(){}
+
+bool Manager::checkPassword(const string password){
+	//TODO
+	return true;
+}
+
+void Manager::changePassword(const string password){
+	//TODO
+}
+
+Club& Manager::getClub(){
+	return *(club_);
+}
+
+string Manager::getUserName(){
+	return userName_;
+}
+
+string Manager::getName(){
+	return name_;
+}
+
+
+

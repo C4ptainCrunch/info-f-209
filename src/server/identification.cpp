79a3d5673fa06b68fdcb03a6c24adef445cd2d47
Cédric Strebelle <cestrebe@ulb.ac.be>
2014-02-07 22:08:38 +0100

    [code] Added sign up fct and some bug fix

--- src/server/identification.cpp
+++ src/server/identification.cpp
@@ -1,6 +1,4 @@
-#include "identification.h"
-	
-}
+#include <Json.h>
 
 bool logIn(char* userName, char * password, const int fd)
 {
@@ -12,10 +10,30 @@ bool logIn(char* userName, char * password, const int fd)
 	    if(usersInfos[log]==password)
 		{
 			logged=true;
-			std::String message="Authentification réussie, bienvenue "+log;
+			std::string message="Authentification réussie, bienvenue "+log;
 			sendTo(fd, message);
 		}
 	}
 	return logged;
 }
-	
+
+bool signUp(char* json, const int fd)
+{
+	bool signedUp=false;
+	std::string log = recieveFrom(fd);
+	std::string password = recieveFrom(fd);
+	Json::Value usersInfos;
+	Json::Reader infosReader;
+	bool parsingSuccess=infoReader.parse(json, usersInfos);
+	if(parsingSuccess)
+	{
+	    if("NULL"==usersInfos.get(log, "NULL"))
+	    {
+		    signedUp=true;
+		    usersInfos[log]=password;
+		    std::string message="Inscription réussie, bienvenue "+log;
+		    sendTo(fd, message);
+		}
+	}
+	return signedUp;
+}

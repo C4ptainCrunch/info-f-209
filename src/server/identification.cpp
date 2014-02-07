e97403df040232045abbb5c0b9a604a5fb862596
Edorior <cestrebe@ulb.ac.be>
2014-02-07 21:16:16 +0100

    [src] Minor change to fct logIn

--- src/server/identification.cpp
+++ src/server/identification.cpp
@@ -1,24 +1,19 @@
-#include <map>
-
-map<std::String, qtd::String>* loadUsersInfos()
-{
-    std::String fileName="UsersInfos.json";
-	map<std::String, std::String>* usersInfos= new map<std::String, std::String>;
-	//parser(usersInfos, fileName);
-	return usersInfos;
+#include "identification.h"
 	
 }
 
-bool logIn(map<std::String, std::String> usersInfos, const int fd)
+bool logIn(char* userName, char * password, const int fd)
 {
 	std::string log = recieveFrom(fd);
 	std::string password = recieveFrom(fd);
 	bool logged=false;
-	if(usersInfos.find(log)!=end())
+	if(userName==log and password)
 	{
 	    if(usersInfos[log]==password)
 		{
 			logged=true;
+			std::String message="Authentification réussie, bienvenue "+log;
+			sendTo(fd, message);
 		}
 	}
 	return logged;

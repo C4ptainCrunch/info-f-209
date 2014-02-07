6712a93219f9ba1f9ce701274cf4b0b9bb040174
Nikita Marchant <nikita.marchant@gmail.com>
2014-02-07 12:46:48 +0100

    [src] Add server minimal implementation

---
+++ src/server/helpers.cpp
@@ -0,0 +1,35 @@
+
+char *get_ip_str(const struct sockaddr *sa, char *s, size_t maxlen=128)
+{
+    switch(sa->sa_family) {
+        case AF_INET:
+            inet_ntop(AF_INET, &(((struct sockaddr_in *)sa)->sin_addr),
+                    s, maxlen);
+            break;
+
+        case AF_INET6:
+            inet_ntop(AF_INET6, &(((struct sockaddr_in6 *)sa)->sin6_addr),
+                    s, maxlen);
+            break;
+
+        default:
+            strncpy(s, "Unknown AF", maxlen);
+            return NULL;
+    }
+
+    return s;
+}
+
+char * addrinfo_to_ip(addrinfo info, char * ip){
+    get_ip_str((struct sockaddr *)info.ai_addr, ip);
+    return ip;
+}
+
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
---
+++ src/server/main.cpp
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#include <thread>
+#include <vector>
+
+#include "helpers.cpp"
+#include "thread.cpp"
+#include "bind.cpp"
+
+
+int main(int argc, char *argv[])
+{
+    if(argc <= 1){
+        printf("Invalid parameters\nUsage : %s port\n", argv[0]);
+        exit(0);
+    }
+    if(atoi(argv[1]) <= 0 || atoi(argv[1]) > 65535){
+        printf("Invalid port\n");
+        exit(0);
+    }
+
+    int new_fd;
+    sockaddr_storage their_addr; // connector's address information
+    socklen_t sin_size;
+    char s[INET6_ADDRSTRLEN];
+
+    int sockfd = bindTo(argv[1]);
+    std::vector<std::thread> pool;
+
+    printf("Waiting for connections...\n");
+
+    while(1) {  // main accept() loop
+        sin_size = sizeof their_addr;
+        new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+        if (new_fd == -1) {
+            perror("accept");
+            continue;
+        }
+
+        inet_ntop(their_addr.ss_family,
+            get_in_addr((struct sockaddr *)&their_addr),
+            s, sizeof s);
+        printf("Got connection from %s\n", s);
+        pool.push_back(std::thread(connection, new_fd));
+        // should join somewhere
+    }
+
+    return 0;
+}

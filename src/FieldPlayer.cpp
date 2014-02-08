9a2b6f87895ed89c51b2c8922ea22a38389d4a95
Bruno Rocha Pereira <brochape@ulb.ac.be>
2014-02-08 22:09:17 +0100

    [code]Added cpp file for FieldPlayer

---
+++ src/FieldPlayer.cpp
@@ -0,0 +1,19 @@
+#include "FieldPlayer.h"
+
+FieldPlayer::FieldPlayer(){}
+
+FieldPlayer::~FieldPlayer(){}
+
+//FieldPlayer::FieldPlayer operator=(FieldPlayer& fieldPlayer){}
+
+void FieldPlayer::move(){}
+
+void FieldPlayer::hitBudger(){}
+
+void FieldPlayer::catchGoldenSnitch(){}
+
+void FieldPlayer::throwQuaffle(){}
+
+void FieldPlayer::catchQuaffle(){}
+
+void FieldPlayer::testMove(){}

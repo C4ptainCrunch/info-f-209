8ae254fc9157d49d0a8dfc3e3e609d5517215003
Tsotne Shonia <tshonia@ulb.ac.be>
2014-02-08 17:53:38 +0100

    [code][client]added getFd to socket API.

--- src/client/Socket.h
+++ src/client/Socket.h
@@ -11,6 +11,7 @@ class Socket {
         Socket(int fd);
         int write(const std::string & message); //Renvoie 0 si succès, 1 si erreur.
         int read(std::string & message); //idem. Modifie param message en lui donnant la valeur du message lu.
+        int getFd();
     private:
         char buffer[BUFF_SIZE];
         int fd;

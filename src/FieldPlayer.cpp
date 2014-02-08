e670d3fa27dd9d0284d25c903835b75e634872ec
Romain Fontaine <rfontain@ulb.ac.be>
2014-02-08 22:21:53 +0100

    [code] added = operator for FieldPlayer and player.

--- src/FieldPlayer.cpp
+++ src/FieldPlayer.cpp
@@ -4,8 +4,19 @@ FieldPlayer::FieldPlayer(){}
 
 FieldPlayer::~FieldPlayer(){}
 
-//FieldPlayer::FieldPlayer operator=(FieldPlayer& fieldPlayer){}
-
+FieldPlayer& FieldPlayer::operator=(Player& player)
+{
+	if (this != &player){
+        speed_ = player.getSpeed();
+        force_ = player.getForce();
+        agility_ = player.getAgility();
+        reflexes_ = player.getReflexes();
+        passPrecision_ = player.getPassPrecision();
+        wounded_ = player.isWounded();
+        inventory_ = player.getInventory();
+    }
+    return *this;
+}
 void FieldPlayer::move(){}
 
 void FieldPlayer::hitBudger(){}

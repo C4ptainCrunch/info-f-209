9523c04de9976c80427685ff89555d87f3a57de3
Tsotne Shonia <tshonia@ulb.ac.be>
2014-02-09 17:00:43 +0100

    [code]Added GameState source and header files, and plugged client to it

--- src/client/client.h
+++ src/client/client.h
@@ -15,20 +15,44 @@
 
 #include <string>
 #include "Socket.h"
+#include "GameState.h"
 
 #define PORT 8000
+
+//Game States
+enum GameStates
+{
+    STATE_NULL,
+    STATE_EXIT,
+    STATE_INTRO,
+
+    STATE_UNLOGGED,
+    STATE_MENU,
+    STATE_MANAGE_PLAYERS,
+    STATE_MANAGE_INFRASTRUCTURES,
+    STATE_AUCTION_HOUSE,
+    STATE_INGAME,
+};
 
 
 class Client{
 public:
     Client();
+    ~Client();
     virtual void connectToName(const std::string name);
     virtual void disconnect();
     virtual int send(const std::string & message); //Renvoie 0 si succès, 1 si erreur.
     virtual int recv(std::string & message); //idem. Modifie param message en lui donnant la valeur du message lu.
+
+    virtual void setNextState(const int newState);
+    virtual void changeState();
 private:
     Socket socket_;
     bool connected_;
+
+    int currentStateID;
+    int nextStateID;
+    GameState *currentState;
 };
 
 #endif //CLIENT_H

d5e1253a03d74a544bf6e3aaf521c9478949c391
Edorior <cestrebe@ulb.ac.be>
2014-02-10 18:43:16 +0100

    [code] Added class Thread

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -10,6 +10,9 @@ int main(int argc, char *argv[])
         printf("Invalid port\n");
         exit(0);
     }
+    
+    // chargement du fichiers avec les infos des users
+    // des infos dans un json, pointé par une variable (const JsonObject * usersInfos)
 
     int new_fd;
     sockaddr_storage their_addr;
@@ -33,7 +36,8 @@ int main(int argc, char *argv[])
             get_in_addr((struct sockaddr *)&their_addr),
             s, sizeof s);
         printf("Got connection from %s\n", s);
-        pool.push_back(std::thread(connection, new_fd));
+        pool.push_back(std::thread(connection, new_fd));  //à remplacer par la ligne en dessous (après test premiers tests client-server)
+        //pool.push_back(std::thread(connection, new_fd, usersInfos))
         // should join somewhere
     }
 

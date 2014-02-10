5f4f836cf1e095b13265c4d8a65cfa00144f61d0
Tsotne Shonia <tshonia@ulb.ac.be>
2014-02-10 16:19:14 +0100

    [code]Progress in game loop - authentification

--- src/client/GameState.cpp
+++ src/client/GameState.cpp
@@ -4,6 +4,10 @@
 #include "client.h"
 #include "GameState.h"
 
+#define DEFAULT 0
+
+#define CONNECTED 1
+
 using namespace std;
 
 GameState::GameState(Client * client)
@@ -13,43 +17,84 @@ GameState::GameState(Client * client)
 
 GameState::~GameState() {}
 
-IntroState::IntroState(Client * client) : GameState(client) {}
-void IntroState::handleEvents()
+IntroState::IntroState(Client * client) : GameState(client)
 {
-    string message;
-    cout<<"Type in 'stop'"<<endl;
-    cin>>message;
-    cout<<message<<endl;
-    if (message == "stop")
-        client_->setNextState(STATE_EXIT);
+    cout<<"BienvenuS dans le systme !"<<endl;
+    client_->setNextState(STATE_UNLOGGED);
 }
+void IntroState::handleEvents() {}
 void IntroState::logic() {}
 void IntroState::display() {}
 
-UnloggedState::UnloggedState(Client * client) : GameState(client) {}
-void UnloggedState::handleEvents() {}
-void UnloggedState::logic() {}
-void UnloggedState::display() {}
+//Unlogged-------------------------------------------------------------------------
 
-MenuState::MenuState(Client * client) : GameState(client) {}
+UnloggedState::UnloggedState(Client * client) : GameState(client)
+{
+    connectionStatus = 0;
+    cout<<"Entrez l'option souhaité avec les paramètres requis délimités par un espace."<<endl;
+    cout<<"Par éxemple : '2 C4 gronoub'"<<endl<<endl;
+    display();
+}
+void UnloggedState::handleEvents()
+{
+    string entry;
+    cin>>entry;
+    //parse entry here
+}
+void UnloggedState::logic()
+{
+    connectionStatus = CONNECTED;
+    if (connectionStatus == CONNECTED)
+        client_->setNextState(STATE_MENU);
+}
+void UnloggedState::display()
+{
+    if (connectionStatus == CONNECTED)
+        cout<<"Connecté."<<endl;
+    else
+    {
+    cout<<"S'enregistrer : 1 - Nom d'utilisateur - Mot de passe"<<endl;
+    cout<<"Se connecter : 2 - Nom d'utilisateur - Mot de passe"<<endl<<endl;
+    }
+}
+
+//Menu-------------------------------------------------------------------------
+
+MenuState::MenuState(Client * client) : GameState(client)
+{
+    cout<<"Hello"<<endl;
+    client_->setNextState(STATE_EXIT);
+}
 void MenuState::handleEvents() {}
 void MenuState::logic() {}
 void MenuState::display() {}
+
+
+//ManagePlayer-------------------------------------------------------------------------
 
 ManagePlayerState::ManagePlayerState(Client * client) : GameState(client) {}
 void ManagePlayerState::handleEvents() {}
 void ManagePlayerState::logic() {}
 void ManagePlayerState::display() {}
 
+
+//ManageInfrastructure-------------------------------------------------------------------------
+
 ManageInfrastructureState::ManageInfrastructureState(Client * client) : GameState(client) {}
 void ManageInfrastructureState::handleEvents() {}
 void ManageInfrastructureState::logic() {}
 void ManageInfrastructureState::display() {}
 
+
+//AuctionHouse-------------------------------------------------------------------------
+
 AuctionHouseState::AuctionHouseState(Client * client) : GameState(client) {}
 void AuctionHouseState::handleEvents() {}
 void AuctionHouseState::logic() {}
 void AuctionHouseState::display() {}
+
+
+//InGame-------------------------------------------------------------------------
 
 InGameState::InGameState(Client * client) : GameState(client) {}
 void InGameState::handleEvents() {}

82c564b91f34dd08e421f99a4410203009c51a47
Cédric Strebelle <cestrebe@ulb.ac.be>
2014-02-10 18:43:16 +0100

    [code] Added class Thread

--- src/server/thread.cpp
+++ src/server/thread.cpp
@@ -1,3 +1,5 @@
+//#include "thread.h"
+
 void connection(const int fd){
     sendTo(fd, "Hello, bite!\n");
     std::string ping = recieveFrom(fd);
@@ -6,3 +8,71 @@ void connection(const int fd){
     close(fd);
 
 }
+
+/*void connection(const int fd, const JsonObject * usersInfos){
+    sendTo(fd, "Hello, bite!\n");
+    bool quit=false
+    while(! quit)
+    {
+        std::string ping = recieveFrom(fd);
+        sendTo(fd, ping);
+        JsonObject json(ping);
+        Thread thread=Thread(fd, usersInfos);
+        if (json.getKey()=="user.login")
+        {
+	        logIn(&json, &thread)
+        }
+        else if (json.getKey()=="user.signup")
+        {
+	        signUp(&json, &thread)
+        }
+        else if (json.getKey()=="user.quit")
+        {
+	        quit=true;
+        }
+        else
+        {
+	        // erreur à gérer
+        }
+        if(thread.getConnected()!="~")
+        {
+	        quidditch(); // boucle infinie jusqu'à ce que le user demande de quitter le jeu
+	        quit=true;
+        }
+    }
+    close(fd);
+
+}*/
+
+/*
+class Thread //classe de l'objet mis en mémoire dans un thread
+{
+	Thread(int fd, const JsonObjet * json)
+	{
+	    userSocket=fd;
+	    userInfos=json;
+	    connected="~"; //valeur sentinelle quand on ne sait pas qui est sur le socket (change au log in)
+
+	}
+	~Thread()
+	{
+	    
+	}
+	bool isKownUser(std::String userName)
+	{
+	    // cherche un user dans une base de donnée et renvoi un bool	    
+	}
+	std::String getConnected()
+	{
+	    return connected;
+	}
+    void setConnected(std::String userName)
+    {
+	    connected=userName;
+    }
+    int getSocket()
+    {
+	    return userSocket;
+    }
+}
+*/

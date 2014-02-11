eee95c409f608ecb8af96e47bcbd47bea541391b
Bruno Rocha Pereira <brochape@ulb.ac.be>
2014-02-11 16:38:26 +0100

    [code] Added the algorithm to determine the form of the field

---
+++ src/matriceOvale.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+
+using namespace std;
+
+int width = 35;
+int lenght = 70;
+
+// void initMatrix(int* matrix){
+//  for (int i = 0; i<width;++i){
+//      for (int j; j<lenght;++i){
+//          matrix[i][j] = 0;
+//      }
+//  }
+// }
+
+// void printMat(int matrix[width][]){
+//  for (int i = 0; i<width;++i){
+//      for (int j; j<lenght;++i){
+//          cout<< matrix[i][j]<< " ";
+//      }
+//      cout<<endl;
+//  }
+// }
+
+
+
+int main(int argc, char const *argv[])
+{
+//-------------------------MATINIT---------------------
+    int matrix[width][lenght];
+    int calc = 0;
+    for (int i = 0; i<width;++i){
+        for (int j=0; j<lenght;++j){
+            matrix[i][j] = 0;
+            if (i%2 ==0){
+                float result =(i-width/2.0)*(i-width/2.0)/(46.0*width/100.0*46.0*width/100.0) + (j-lenght/2.0)*(j-lenght/2.0)/(46.0*lenght/100*46.0*lenght/100); 
+                if (result>1){
+                    matrix[i][j] = 1;
+                    calc+=1;
+                }
+            }
+            else{
+                float result = (i-width/2.0)*(i-width/2.0)/(46.0*width/100*46.0*width/100) + (j-lenght/2.0)*(j-lenght/2.0)/(46.0*lenght/100.0*46.0*lenght/100) -1/2;
+                if (result >1){
+                    matrix[i][j] = 1;
+                    calc+=1;
+                }
+            }
+//----------------------------GOALS---------------------------------
+            if( i == width/2){
+                if (j==lenght/15+lenght/20 or j==14*lenght/15-lenght/20){
+                    matrix[i][j] = 2;
+                }
+            }
+            if (i == width/2-width/15 or i==width/2+width/15){
+                if(j==2*lenght/15 or j==13*lenght/15){
+                    matrix[i][j] = 2;
+                }
+            }
+        }
+    }
+//---------------------------PRINT----------------------------
+    for (int i = 0; i<width;++i){
+        if (i%2 != 0){
+            cout<<" ";
+        }
+        for (int j=0; j<lenght;++j){
+            
+            if (matrix[i][j] == 0){
+                cout<< "\u2B21"<<" ";
+            }
+            else{
+                cout <<"\u2B22"<<" ";
+            }
+            
+            //cout<< matrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<calc<<endl;//nbre de cases utilisables
+    return 0;
+}
+

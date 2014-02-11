400e13a9979c25bbdaca6cb66adf02c1a48df389
Nikita Marchant <nikita.marchant@gmail.com>
2014-02-12 00:55:02 +0100

    [src/server] Comment some code in thread.h so it compiles

--- src/server/thread.h
+++ src/server/thread.h
@@ -10,21 +10,21 @@
 
 void connection(const int socket);
 
-class Thread //classe de l'objet mis en mémoire dans un thread
-{
-public:
-	Thread(int fd, const JsonObjet * usersInfos);
-	~Thread();
-	bool isKownUser(std::String userName);
-	std::String getConnected();
-	void setConnected(std::String userName);
-	int getSocket();
+// class Thread //classe de l'objet mis en mémoire dans un thread
+// {
+// public:
+// 	Thread(int fd, const JsonObjet * usersInfos);
+// 	~Thread();
+// 	bool isKownUser(std::String userName);
+// 	std::String getConnected();
+// 	void setConnected(std::String userName);
+// 	int getSocket();
 
-private:
-	int userSocket;   // socket d'écoute du thread
-	const JsonObject * usersInfos;// pointeur vers une structure contenant les log/password de tous les users
-	std::String connected; // log du user connecté au socket du thread
-};
+// private:
+// 	int userSocket;   // socket d'écoute du thread
+// 	const JsonObject * usersInfos;// pointeur vers une structure contenant les log/password de tous les users
+// 	std::String connected; // log du user connecté au socket du thread
+// };
 
-// dériver Thread en ServerThread, AuctionThread, MatchThread, ManageThread, ... ?
+// // dériver Thread en ServerThread, AuctionThread, MatchThread, ManageThread, ... ?
 #endif //THREAD_H

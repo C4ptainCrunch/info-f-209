4713a8c1a58868acc6184c58872dcc02b6bec650
Romain Fontaine <rfontain@ulb.ac.be>
2014-02-11 17:30:22 +0100

    [code] Lines shorter and sum more readable.

--- src/matriceOvale.cpp
+++ src/matriceOvale.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -26,27 +27,23 @@ int lenght = 70;
 
 int main(int argc, char const *argv[])
 {
-//-------------------------MATINIT---------------------
+
+    //-------------------------MATINIT---------------------
     int matrix[width][lenght];
-    int calc = 0;
+    int calc=0;
     for (int i = 0; i<width;++i){
         for (int j=0; j<lenght;++j){
             matrix[i][j] = 0;
-            if (i%2 ==0){
-                float result =(i-width/2.0)*(i-width/2.0)/(46.0*width/100.0*46.0*width/100.0) + (j-lenght/2.0)*(j-lenght/2.0)/(46.0*lenght/100*46.0*lenght/100); 
-                if (result>1){
-                    matrix[i][j] = 1;
-                    calc+=1;
-                }
+            float result = pow(i-width/2.0, 2)/pow(46.0*width/100.0, 2);
+            result+= pow(j-lenght/2.0, 2)/pow(46.0*lenght/100, 2);
+            if (i%2 !=0){
+                result -= 1/2;
             }
-            else{
-                float result = (i-width/2.0)*(i-width/2.0)/(46.0*width/100*46.0*width/100) + (j-lenght/2.0)*(j-lenght/2.0)/(46.0*lenght/100.0*46.0*lenght/100) -1/2;
-                if (result >1){
-                    matrix[i][j] = 1;
-                    calc+=1;
-                }
+            if (result>1){
+                matrix[i][j] = 1;
+                calc++;
             }
-//----------------------------GOALS---------------------------------
+    //----------------------------GOALS---------------------------------
             if( i == width/2){
                 if (j==lenght/15+lenght/20 or j==14*lenght/15-lenght/20){
                     matrix[i][j] = 2;
@@ -59,20 +56,20 @@ int main(int argc, char const *argv[])
             }
         }
     }
-//---------------------------PRINT----------------------------
+    //---------------------------PRINT----------------------------
     for (int i = 0; i<width;++i){
         if (i%2 != 0){
             cout<<" ";
         }
         for (int j=0; j<lenght;++j){
-            
+
             if (matrix[i][j] == 0){
                 cout<< "\u2B21"<<" ";
             }
             else{
                 cout <<"\u2B22"<<" ";
             }
-            
+
             //cout<< matrix[i][j]<<" ";
         }
         cout<<endl;

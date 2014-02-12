ef6ba404484d7aa5bda62741374d8b4ab40b28fd
Cédric Strebelle <cestrebe@ulb.ac.be>
2014-02-12 18:59:06 +0100

    [code] Added files fcts, adapted logIn and signUp to it, and cleaned stuffs

--- src/server/menu.cpp
+++ src/server/menu.cpp
@@ -2,72 +2,72 @@
 
 void inviteToMatch(char * message, Thread * thread)
 {
-	char * reponseMessage;
-	JsonObject json(message); 
+    char * reponseMessage;
+    JsonObject json(message);
 }
 
 void reponseToMatchInvitation(char * message, Thread * thread)
 {
-	char * reponseMessage;
-	JsonObject json(message); 
+    char * reponseMessage;
+    JsonObject json(message); 
 }
 
 void viewUserList(char * message, Thread * thread)
 {
-	char * reponseMessage;
-	JsonObject json(message);     
+    char * reponseMessage;
+    JsonObject json(message);     
 }
 
 void goToManagement(char * message, Thread * thread) //amène à l'écran de gestion du club
 {
-	char * reponseMessage;
-	JsonObject json(message); 
+    char * reponseMessage;
+    JsonObject json(message); 
 }
 
 void buildNewInstallation(char * message, Thread * thread)
 {
-	char * reponseMessage;
-	JsonObject json(message); 
+    char * reponseMessage;
+    JsonObject json(message); 
 }
 
 void manageInstallation(char * message, Thread * thread) //amène à la gestion de l'installation
 {
-	char * reponseMessage;
-	JsonObject json(message); 
+    char * reponseMessage;
+    JsonObject json(message); 
 }
 
 void manageTeam()
 {
-	char * reponseMessage;
-	JsonObject json(message); 
+    char * reponseMessage;
+    JsonObject json(message); 
 }
 
 void goToAuctionRoom(char * message, Thread * thread)
 {
-	char * reponseMessage;
-	JsonObject json(message); 
+    char * reponseMessage;
+    JsonObject json(message); 
 }
 
 void viewAuctions(char * message, Thread * thread)
 {
-	char * reponseMessage;
-	JsonObject json(message); 
+    char * reponseMessage;
+    JsonObject json(message); 
 }
 
 void bidToAuction(char * message, Thread * thread) // nécessite un id par enchère. l'id de l'enchère doit etre mentionné dans le message
 {
-	char * reponseMessage;
-	JsonObject json(message); 
+    char * reponseMessage;
+    JsonObject json(message); 
 }
 
 void viewUserBids(char * message, Thread * thread)
 {
-	char * reponseMessage;
-	JsonObject json(message); 
+    char * reponseMessage;
+    JsonObject json(message); 
 }
 
 void makeAuction(char * message, Thread * thread)
 {
-	char * reponseMessage;
-	JsonObject json(message);     
+    char * reponseMessage;
+    JsonObject json(message);     
 }

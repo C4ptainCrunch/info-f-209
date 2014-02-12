413af8bb132676fc78eac1d43acedd98bad7b1ea
Bruno Rocha Pereira <brochape@ulb.ac.be>
2014-02-12 15:53:34 +0100

    [code]Added the grid generator in Match

--- src/matriceOvale.cpp
+++ src/matriceOvale.cpp
@@ -31,15 +31,16 @@ int main(int argc, char const *argv[])
     //-------------------------MATINIT---------------------
     int matrix[width][lenght];
     int calc=0;//Nombre de cases utilisables
-    float halfLenght = lenght/2.0;
-    float halfWidth = width/2.0;
     float diameterFactor = 46.0/100.0;// Normalement c'est la moitié de la longueur/largeur 
     int delta = 1/2; //Delta qui permet d'éviter les bugs lors de l'affichage de la matrice
+
     for (int i = 0; i<width;++i){
         for (int j=0; j<lenght;++j){
             matrix[i][j] = 0;
-            float result = pow(i-halfWidth, 2)/pow(diameterFactor*width, 2);// equation d'une ellipse non centrée : (x-h)²/a² + (x-k)²/b²
-            result+= pow(j-halfLenght, 2)/pow(diameterFactor*lenght, 2);//avec x = i, h et k sont les coord du centre, a et b les demi longueurs de l'ellipse
+            // equation d'une ellipse non centrée : (x-h)²/a² + (x-k)²/b²
+            //avec x = i, h et k sont les coord du centre, a et b les demi longueurs de l'ellipse
+            float result = pow(i-width/2.0, 2)/pow(diameterFactor*width, 2);
+            result+= pow(j-lenght/2.0, 2)/pow(diameterFactor*lenght, 2);
             if (i%2 !=0){
                 result -= delta;
             }
@@ -49,12 +50,12 @@ int main(int argc, char const *argv[])
             }
     //----------------------------GOALS---------------------------------
             if( i == width/2){
-                if (j==lenght/15+lenght/20 or j==14*lenght/15-lenght/20){
+                if (j == lenght/15 + lenght/20 or j == lenght*14/15 - lenght/20){
                     matrix[i][j] = 2;//goal central
                 }
             }
-            if (i == width/2-width/15 or i==width/2+width/15){
-                if(j==2*lenght/15 or j==13*lenght/15){
+            if (i == width/2 - width/15 or i== width/2 + width/15){
+                if(j == 2 * lenght/15 or j == 13 * lenght/15){
                     matrix[i][j] = 2;//goals latéraux
                 }
             }

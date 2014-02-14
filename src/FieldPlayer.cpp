049ece8516542ac2f518920993591246ebf7cc2f
Bruno Rocha Pereira <brochape@ulb.ac.be>
2014-02-14 16:21:37 +0100

    [code] Added the copy and the default constructor

--- src/FieldPlayer.cpp
+++ src/FieldPlayer.cpp
@@ -3,7 +3,21 @@
 
 FieldPlayer::FieldPlayer(int role): role_(role){}
 
+FieldPlayer::FieldPlayer(): role_(0){}
+
 FieldPlayer::~FieldPlayer(){}
+
+FieldPlayer::FieldPlayer(NonFieldPlayer& nonFieldPlayer, int role){
+        speed_ = nonFieldPlayer.getSpeed();
+        force_ = nonFieldPlayer.getForce();
+        agility_ = nonFieldPlayer.getAgility();
+        reflexes_ = nonFieldPlayer.getReflexes();
+        passPrecision_ = nonFieldPlayer.getPassPrecision();
+        wounded_ = nonFieldPlayer.isWounded();
+        inventory_ = nonFieldPlayer.getInventory();
+        role_ = role;
+
+}
 
 FieldPlayer& FieldPlayer::operator=(Player& player)
 {
@@ -29,6 +43,10 @@ void FieldPlayer::throwQuaffle(){}
 void FieldPlayer::catchQuaffle(){}
 
 void FieldPlayer::testMove(){}
+
+void FieldPlayer::setRole(int role){
+    role_ = role;
+}
 
 int FieldPlayer::getRole(){
     return role_;

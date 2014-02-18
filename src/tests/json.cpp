51ddf1ae7c11084d9abfc6037c89b4127c7cb613
Nikita Marchant <nimarcha@ulb.ac.be>
2014-02-18 16:47:14 +0100

    [code/tests] Add a whole bunch of macros to help testing

---
+++ src/tests/json.cpp
@@ -0,0 +1,34 @@
+#include "libtest.h"
+
+using namespace std;
+//#include "../lib/json/json.h"
+
+void test_empty_dict(){
+    cout << "plip" << endl;
+    // string message = "{}";
+    // int i = 0;
+
+    // JsonValue * value = JsonValue::fromString(message, i)
+    // ASSERT_NOT_NULL(value);
+
+    // JsonDict* dict_p = JDICT(value);
+    // ASSERT_NOT_NULL(dict_p);
+
+    // ASSERT_EQUAL(dict_p->size(), 0);
+}
+
+void test_empty_list(){
+    // string message = "{}";
+    // int i = 0;
+
+    // JsonValue * value = JsonValue::fromString(message, i)
+    // ASSERT_NOT_NULL(value);
+
+    // JsonDict* dict_p = JDICT(value);
+    // ASSERT_NOT_NULL(dict_p);
+
+    // ASSERT_EQUAL(dict_p->size(), 0);
+}
+
+#define TESTVEC {T(test_empty_dict), T(test_empty_list)}
+TEST();

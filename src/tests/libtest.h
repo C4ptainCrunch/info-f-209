7232aa4c742d4797b7c21f83a76cd1776c5c083f
Romain Fontaine <rfontain@ulb.ac.be>
2014-02-18 19:30:57 +0100

    [code/test] added new assert fonctions (assertFalse; assertNull; assertNotNull; assertEqual).

--- src/tests/libtest.h
+++ src/tests/libtest.h
@@ -17,6 +17,14 @@ int test(std::vector<func_struct> testvec);
 
 void assert(bool value, std::string message="Assert failed");
 
+void assertFalse(bool value, std::string message="");
+template<typename T>
+void assertNull(T value, std::string message="");
+template<typename T>
+void assertNotNull(T value, std::string message="");
+template<typename T, typename U>
+void assertEqual(T value1, U value2, std::string message="");
+
 class AssertFail: public std::exception {
     public:
         AssertFail(std::string message);

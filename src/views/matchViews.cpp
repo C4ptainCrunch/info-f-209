3cbb794b589e5d399ef76cf7a300580eceb90029
Cédric Strebelle <cestrebe@ulb.ac.be>
2014-02-19 12:46:17 +0100

    [code/json] Some fixes

--- src/views/matchViews.cpp
+++ src/views/matchViews.cpp
@@ -1,31 +1,31 @@
-void playerSelection(JsonDict * json, UserHandler * thread){
+void playerSelection(JsonValue * json, UserHandler * thread){
     char * reponse;
 }
 
-void playerAction(JsonDict * json, UserHandler * thread){ // si un joueur est selectionné, montre les actions possibles (mouvement, taper cognard, passe, ...)
+void playerAction(JsonValue * json, UserHandler * thread){ // si un joueur est selectionné, montre les actions possibles (mouvement, taper cognard, passe, ...)
     char * reponse;
 }
 
-void selectCase(JsonDict * json, UserHandler * thread){
+void selectCase(JsonValue * json, UserHandler * thread){
     char * reponse;
 }
 
-void getPlayerDeplacement(JsonDict * json, UserHandler * thread){ // reçoit l'entrée de la destination d'un joueur se déplaçant
+void getPlayerDeplacement(JsonValue * json, UserHandler * thread){ // reçoit l'entrée de la destination d'un joueur se déplaçant
     char * reponse;
 }
 
-void playerCatcheSnitch(JsonDict * json, UserHandler * thread){
+void playerCatcheSnitch(JsonValue * json, UserHandler * thread){
     char * reponse;
 }
 
-void playerHitBudger(JsonDict * json, UserHandler * thread){ // reçoit l'action frapper cognard
+void playerHitBudger(JsonValue * json, UserHandler * thread){ // reçoit l'action frapper cognard
     char * reponse;
 }
 
-void doPass(JsonDict * json, UserHandler * thread){ //reçoit l'action de tentative de passe vers un autre joueur de l'équipe
+void doPass(JsonValue * json, UserHandler * thread){ //reçoit l'action de tentative de passe vers un autre joueur de l'équipe
     char * reponse;
 }
 
-void doInterception(JsonDict * json, UserHandler * thread){
+void doInterception(JsonValue * json, UserHandler * thread){
     char * reponse;
 }
--- src/views/menu.cpp
+++ src/views/menu.cpp
@@ -1,49 +1,49 @@
 // TO DO : mettre communication entre thread (pour les erreurs)
 
-void inviteToMatch(JsonDict * json, UserHandler * thread){
+void inviteToMatch(JsonValue * json, UserHandler * thread){
     char * reponse;
 }
 
-void reponseToMatchInvitation(JsonDict * json, UserHandler * thread){
+void reponseToMatchInvitation(JsonValue * json, UserHandler * thread){
     char * reponse;
 }
 
-void viewUserList(JsonDict * json, UserHandler * thread){
+void viewUserList(JsonValue * json, UserHandler * thread){
     char * reponse;
 }
 
-void goToManagement(JsonDict * json, UserHandler * thread){
+void goToManagement(JsonValue * json, UserHandler * thread){
     char * reponse;
 } //amène à l'écran de gestion du club
 
-void buildNewInstallation(JsonDict * json, UserHandler * thread){
+void buildNewInstallation(JsonValue * json, UserHandler * thread){
     char * reponse;
 }
 
-void manageInstallation(JsonDict * json, UserHandler * thread){
+void manageInstallation(JsonValue * json, UserHandler * thread){
     char * reponse;
 } //amène à la gestion de l'installation
 
-void manageTeam(JsonDict * json, UserHandler * thread){
+void manageTeam(JsonValue * json, UserHandler * thread){
     char * reponse;
 }
 
-void goToAuctionRoom(JsonDict * json, UserHandler * thread){
+void goToAuctionRoom(JsonValue * json, UserHandler * thread){
     char * reponse;
 }
 
-void viewAuctions(JsonDict * json, UserHandler * thread){
+void viewAuctions(JsonValue * json, UserHandler * thread){
     char * reponse;
 }
 
-void bidToAuction(JsonDict * json, UserHandler * thread){
+void bidToAuction(JsonValue * json, UserHandler * thread){
     char * reponse;
 } // nécessite un id par enchère. l'id de l'enchère doit etre mentionné dans le message
 
-void viewUserBids(JsonDict * json, UserHandler * thread){
+void viewUserBids(JsonValue * json, UserHandler * thread){
     char * reponse;
 }
 
-void makeAuction(JsonDict * json, UserHandler * thread){
+void makeAuction(JsonValue * json, UserHandler * thread){
     char * reponse;
 }

18e238a429f0bb8be7a8c902a098d5fb8ac2b252
Bruno Rocha Pereira <brochape@ulb.ac.be>
2014-02-20 17:33:18 +0100

    [code] Added the position attribute to Ball

--- src/models/Ball.cpp
+++ src/models/Ball.cpp
@@ -5,3 +5,11 @@ Ball::Ball(int speed):speed_(speed){ }
 Ball::~Ball(){
 
 }
+
+Position Ball::getPosition(){
+	return position_;
+}
+
+Ball::setPosition(Position position){
+	position_ = position;
+}
--- src/models/Ball.h
+++ src/models/Ball.h
@@ -1,13 +1,18 @@
 #ifndef BALL_H
 #define BALL_H
 
+#include "Position.h"
+
 class Ball {
 
     public:
         Ball(int speed);
         virtual ~Ball();
+        Position getPosition();
+        void setPosition(Position position);
 
     protected:
         int speed_;
+        Position position_;
 };
 #endif // BALL_H

d0f3b948e4931bcc827ada0dd2392e2c54e95786
Nikita Marchant <nimarcha@ulb.ac.be>
2014-03-06 11:54:23 +0100

    Fix missing #ifdef in src/common/lib/json/utils.h

--- src/common/lib/json/utils.h
+++ src/common/lib/json/utils.h
@@ -1,3 +1,6 @@
+#ifndef JSON_UTILS_H
+#define JSON_UTILS_H
+
 #include "ParseError.h"
 #include <vector>
 #include <string>
@@ -5,3 +8,5 @@
 int skip_whitespace(std::string message, int start);
 int skip_colon(std::string message, int start);
 std::string cut_from(std::string message, int from);
+
+#endif

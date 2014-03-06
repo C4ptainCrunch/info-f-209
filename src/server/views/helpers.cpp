100e09d11e770ae604baf678cb009c4b0c9bd2c4
Nikita Marchant <nimarcha@ulb.ac.be>
2014-03-06 12:07:50 +0100

    Add an error code with sendFail

--- src/server/views/helpers.cpp
+++ src/server/views/helpers.cpp
@@ -7,7 +7,9 @@ void sendFail(UserHandler * handler, int errorcode, string topic, string message
 
     JsonBool b = JsonBool(false);
     JsonString s = JsonString(message);
+    JsonInt i = JsonInt(errorcode);
     answer.add("success", &b);
     answer.add("reason", &s);
+    answer.add("code", &i);
     handler->writeToClient(topic, &answer);
 }

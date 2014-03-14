8ee4b4640875bad979f613cec51196ae5b2a7721
Bruno Rocha Pereira <brochape@ulb.ac.be>
2014-03-14 17:29:05 +0100

    added missing files (bis)

---
+++ src/server/views/management.h
@@ -0,0 +1,22 @@
+#ifndef SVIEWMANAGEMENT_H
+#define SVIEWMANAGEMENT_H
+
+#include <cstdlib>
+#include <vector>
+
+#include "helpers.h"
+#include "../../common/lib/json/json.h"
+#include "../../common/lib/json/helpers.h"
+#include "../../common/lib/exception/BadRequest.h"
+
+class UserHandler;
+
+namespace views {
+void playerlist(JsonValue * message, UserHandler * handler);
+}
+
+#include "../UserHandler.h"
+#include "../../common/lib/file/file.h"
+#include "../../common/models/Manager.h"
+
+#endif // SVIEWMANAGEMENT_H

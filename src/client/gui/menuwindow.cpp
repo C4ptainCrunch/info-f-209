e312e3f8d363fc67c22a23918fb06d38101ca0b7
Bruno Rocha Pereira <brochape@ulb.ac.be>
2014-04-05 02:15:16 +0200

    [GUI] Changed the firstMenu variable

--- src/client/gui/menuwindow.cpp
+++ src/client/gui/menuwindow.cpp
@@ -4,9 +4,6 @@ using namespace std;
 
 MenuWindow::MenuWindow(MainWindow * parent):
     QWidget(parent), parent_(parent) {
-    bool firstMenu = parent_->isFirstMenu();
-    parent_->setFirstMenu(false);
-    cout << "PREMIER MENU : " << firstMenu;
     //-------------------------SIZE SETTINGS---------------------------
     this->setFixedHeight(720);
     this->setFixedWidth(1280);
@@ -30,16 +27,16 @@ MenuWindow::MenuWindow(MainWindow * parent):
 
     //---------------------------OPPONENT CHOICE-------------------
     list = new QComboBox(matchLauncherWidget);
-    //this->askConnectedListRefresh();
+
 
     QPushButton * startMatchButton = new QPushButton("DEFIER", matchLauncherWidget);
 
     startMatchButton->setMinimumHeight(40);
     startMatchButton->setStyleSheet(" font-weight: bold; font-size: 18pt;");
     QPushButton * refreshButton = new QPushButton("Rafraichir", matchLauncherWidget);
-    //if(firstMenu){
+
     QObject::disconnect(refreshButton, 0, 0, 0);
-    //}
+
     QObject::connect(refreshButton, SIGNAL(clicked()), this, SLOT(askConnectedListRefresh()));
 
     matchLauncherLayout->addWidget(list, 0, 0, 1, 3);
@@ -54,23 +51,23 @@ MenuWindow::MenuWindow(MainWindow * parent):
     matchLauncherWidget->setLayout(matchLauncherLayout);
 
     //--------------------CHALLENGE BUTTON--------------------------
-    //if(firstMenu){
+
     QObject::disconnect(startMatchButton, 0, 0, 0);
-    //}
+
     QObject::connect(startMatchButton, SIGNAL(clicked()), this, SLOT(sendChallenge()));
 
     //-------------------AUCTIONHOUSE BUTTON-------------------------
     auctionHouseButton = new QPushButton("Encheres");
     auctionHouseButton->setMinimumHeight(60);
-    //if(firstMenu){
+
     QObject::disconnect(auctionHouseButton, 0, 0, 0);
     connect(auctionHouseButton, SIGNAL(clicked()), this, SLOT(auctionHouse()));
-    //}
+
 
     //-----------------TEAM HANDLING BUTTON--------------------------
     teamHandlingButton = new QPushButton("Gestion de Team");
     teamHandlingButton->setMinimumHeight(60);
-    //if(!firstMenu){
+
     QObject::disconnect(teamHandlingButton, 0, 0, 0);
 
     connect(teamHandlingButton, SIGNAL(clicked()), this, SLOT(handlePlayers()));
@@ -95,9 +92,8 @@ MenuWindow::MenuWindow(MainWindow * parent):
 
     //-----------------------CUSTOM SIGNALS CONNECTION--------------------
 
-    //if(firstMenu){
+
     QObject::disconnect(parent_, SIGNAL(startMatch(Match *, bool, int)), 0, 0);
-    //QObject::disconnect(0,0,this,SLOT(refreshConnectedList(std::vector<std::string> *)));
     QObject::connect(parent_, SIGNAL(startMatch(Match *, bool, int)), this, SLOT(startMatch(Match *, bool, int)));
     QObject::connect(parent_, SIGNAL(userList(std::vector<std::string> *)), this, SLOT(refreshConnectedList(std::vector<std::string> *)));
     //}

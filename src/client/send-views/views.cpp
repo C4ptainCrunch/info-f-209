78b083ed05fd83ffad5599ba77df34f4246cd497
Nikita Marchant <nimarcha@ulb.ac.be>
2014-04-07 23:47:11 +0200

    Remove old comments and debug info

--- src/client/send-views/views.cpp
+++ src/client/send-views/views.cpp
@@ -66,7 +66,6 @@ void endTurn(Socket * s, vector<Way> chosenWays) {
 }
 
 void surrenders(Socket * s, int matchID) {
-    cout << "SURREND? " << matchID << endl;
     JsonInt message = JsonInt(matchID);
 
     writeToServer(s, "surrender", message.toString());
